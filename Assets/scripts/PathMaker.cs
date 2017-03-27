64b13e43b8e55c000d6d643c1720858744ecda75
Snippy Nguyen <chevy666camaro@gmail.com>
2017-03-27 01:46:18 -0400

    First Attempt at PG
    Tile spawns upwards indefinitely. Or it stops after 500, I can't tell.
    Need to work out the code logic. Bloody for loops.

---
+++ Assets/scripts/PathMaker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMaker : MonoBehaviour {
+
+	public Transform floorPrefab;
+	public Transform pathmakerSpherePrefab; 
+
+	List<Transform> pathmakerList = new List<Transform>();
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int counter = 0; 
+
+		if (counter < 50) {
+			if (Random.Range (0.0f, 1.0f) > 0.0f) {
+				if (Random.Range (0.25f, 0.5f) < 0.25f) {
+					transform.Rotate (0f, 90f, 0f);
+				}
+			} else if (Random.Range (0.0f, 1.0f) > 0.25f) {
+				if (Random.Range (0.0f, 1.0f) < 0.5f) {
+					transform.Rotate (0f, -90f, 0f);
+				}
+			} else if (Random.Range (0.99f, 1.0f) > 0.99f) {
+				Instantiate (pathmakerSpherePrefab, transform.position, Quaternion.identity);
+			}
+			Instantiate (floorPrefab, transform.position, Quaternion.identity);
+			transform.Translate (0f, 5f, 0f);
+		}
+
+		counter++; 
+
+	}
+}
