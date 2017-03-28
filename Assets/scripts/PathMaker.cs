c82601b8cbe463bb66528bda348833a922fc9f80
Snippy Nguyen <chevy666camaro@gmail.com>
2017-03-27 23:01:55 -0400

    Indefinite Spawning worked
    I feel so stupid, it's been spawing upwards because I told it to move on the y axis. Wasted half an hour on this, kill me.
    Now I need to figure out how to stop it at 500 tiles
    Also need to add a replay button.

--- Assets/scripts/PathMaker.cs
+++ Assets/scripts/PathMaker.cs
@@ -7,31 +7,57 @@ public class PathMaker : MonoBehaviour {
 	public Transform floorPrefab;
 	public Transform pathmakerSpherePrefab; 
 
-	List<Transform> pathmakerList = new List<Transform>();
+
+
+	//List<Transform> pathmakerList = new List<Transform>();
 
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		int counter = 0; 
+		int floorCounter = 0; 
 
-		if (counter < 50) {
-			if (Random.Range (0.0f, 1.0f) > 0.0f) {
-				if (Random.Range (0.25f, 0.5f) < 0.25f) {
-					transform.Rotate (0f, 90f, 0f);
-				}
-			} else if (Random.Range (0.0f, 1.0f) > 0.25f) {
-				if (Random.Range (0.0f, 1.0f) < 0.5f) {
-					transform.Rotate (0f, -90f, 0f);
-				}
-			} else if (Random.Range (0.99f, 1.0f) > 0.99f) {
+		while (floorCounter < 10) {
+			float myRandomNumber = Random.Range (0.0f, 1.0f); 
+			if (myRandomNumber < 0.25f) {
+				transform.Rotate (0f, 90f, 0f);
+			} else if (myRandomNumber >= 0.25f && myRandomNumber < 0.5f) {
+				transform.Rotate (0f, -90f, 0f);
+			} else if (myRandomNumber >= 0.99f && myRandomNumber < 1.0f) {
 				Instantiate (pathmakerSpherePrefab, transform.position, Quaternion.identity);
 			}
+
 			Instantiate (floorPrefab, transform.position, Quaternion.identity);
-			transform.Translate (0f, 5f, 0f);
+			transform.Translate (0f, 0f, 5f);
+			floorCounter++;
 		}
 
-		counter++; 
+
+
 
 	}
 }
+
+
+
+
+
+
+
+//			if (Random.Range (0.0f, 1.0f) > 0.0f) {
+//				if (Random.Range (0.25f, 0.5f) < 0.25f) {
+//					transform.Rotate (0f, 90f, 0f);
+//				}
+//			} else if (Random.Range (0.0f, 1.0f) > 0.25f) {
+//				if (Random.Range (0.0f, 1.0f) < 0.5f) {
+//					transform.Rotate (0f, -90f, 0f);
+//				}
+//			} else if (Random.Range (0.99f, 1.0f) > 0.99f) {
+//				Instantiate (pathmakerSpherePrefab, transform.position, Quaternion.identity);
+//			}
+//			Instantiate (floorPrefab, transform.position, Quaternion.identity);
+//			transform.Translate (0f, 5f, 0f);
+//		}
+//
+//		counter++; 
+
